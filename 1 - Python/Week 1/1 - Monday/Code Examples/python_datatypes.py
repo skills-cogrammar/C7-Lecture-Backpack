db0559bba48bcfcf4a452dc8382c2cc440529576
Serge-Mentor <79256268+Serge-Mentor@users.noreply.github.com>
2024-03-12 08:21:30 +0200

    Add files via upload

---
+++ 1 - Python/Week 1/1 - Monday/Code Examples/python_datatypes.py
@@ -0,0 +1,28 @@
+# Integer
+num_one = 100
+num_two = 40
+num_three = 9
+
+# print(num_one + num_two)
+
+# Floats
+float_one = 12.34
+float_two = 0.003
+
+print(float_one + float_two)
+
+print(num_one + float_one)
+
+# String
+my_name = "Serge"
+
+my_fav_meal = 'sugar'
+
+random_string = "The cat has stolen the ghost's soul."
+
+# Boolean
+is_poke_out = True
+is_poke_hungry = False
+
+# Nonetype
+default_value = None
