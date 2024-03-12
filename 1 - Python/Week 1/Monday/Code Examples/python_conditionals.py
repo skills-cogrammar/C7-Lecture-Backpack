9e6329d284aa152f280c6dd84df7f4ecb523a762
Serge-Mentor <79256268+Serge-Mentor@users.noreply.github.com>
2024-03-12 08:02:49 +0200

    Add files via upload

---
+++ 1 - Python/Week 1/Monday/Code Examples/python_conditionals.py
@@ -0,0 +1,20 @@
+my_name = "Mewtwo"
+is_poke_out = True
+
+# If statement
+if my_name == "Pikachu":
+    print("This is not serge")
+
+# Elif Statement
+elif my_name == "Serge":
+    print("AHh serge you are not a pokemon")
+
+    if is_poke_out == True:
+        print("you may be a pokemon lowkey")
+
+elif my_name == "Mewtwo":
+    print("YOu are a legendary pokemon")
+
+# Else Statement
+else:
+    print("you must be the chosen one ! Slowpoke!")
