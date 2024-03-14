bd07612a819f16415f9d681f11c2252bce29ff3e
zahir-cogrammar <zahirj@cogrammar.com>
2024-03-14 22:08:06 +0500

    Added tutorial content

---
+++ 4 - Full Stack Web Development (WD)/Week1/Tutorial/tutorial.js
@@ -0,0 +1,32 @@
+// This is how you make a variable in javascript
+// This is a numerical data type
+// let numberOfCats = 9;
+// console.log("Number of cats: ", numberOfCats);
+
+// let distance = 9.6;
+// console.log("Distance: ", distance);
+
+// This is a string data type
+// let fullName = "John Doe";
+// console.log("Name: ", fullName);
+
+// This is how you make a boolean data type
+// Remembe: A boolean data type can be either true or false
+// let doorOpen = true;
+// console.log("doorOpen is a boolean data type: ", doorOpen);
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+// console.log("Numbers array: ", numbers);
+// let items = ["Charlie", 6, true, 9, 4, 6, 5, 3];
+// console.log("Items array: ", items);
+
+// let car = {
+//   numberofWheels: 4,
+//   modelName: "Model T",
+//   manufacturerName: "Tesla",
+// };
+
+// console.log("Car object: ", car);
+
+// const PI = 3.14;
+// PI = 2.2;
