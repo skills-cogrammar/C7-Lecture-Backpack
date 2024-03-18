3797213920649091a25714a4f5c8ade9a182f982
Serge-Thomas <79256268+Serge-Thomas@users.noreply.github.com>
2024-03-18 23:14:23 +0200

    Add files via upload

---
+++ 1 - Python/Week 2/1 - Monday/sesh_example_1.py
@@ -0,0 +1,25 @@
+# Example 1
+# Random Function
+# def poke_in_bag(poke1, poke2):
+#     print(f"Pokemon 1: {poke1}.\nPokemon2: {poke2}")
+#     print(f"You have an extra pokemon: {global_poke}")
+
+
+# global_poke = "MewTwo"
+
+# # function call
+# poke_in_bag("slowpoke", "gengar")
+
+# # printing out
+# print(global_poke)
+
+# Example 2
+
+# def my_function():
+#     global y
+#     y = "Look captain! A whale!"
+#     print(y)
+
+
+# my_function()
+# print(y)
---
+++ 1 - Python/Week 2/1 - Monday/sesh_example_2.py
@@ -0,0 +1,23 @@
+# Example 1
+# Error example
+
+# def divide(a, b):
+#     return a / b
+
+
+# def main_function():
+#     result = divide(10, 0)
+#     print(result)
+
+
+# main_function()
+
+# Example 2
+
+def string_builder(word1, word2, word3, word4):
+    return f"{word1 }-{word2}-{word3}-{word4}"
+
+
+new_random = string_builder("socio-economic", str(4), "feels", 45.7)
+
+print(new_random)
