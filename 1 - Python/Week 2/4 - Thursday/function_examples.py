83b4254e3afe5c766c0320fc0b48826de028cbf2
Serge-Thomas <79256268+Serge-Thomas@users.noreply.github.com>
2024-03-21 23:08:38 +0200

    Add files via upload

---
+++ 1 - Python/Week 2/4 - Thursday/function_examples.py
@@ -0,0 +1,40 @@
+
+
+# Example 1
+
+# print("Hello World")
+
+# random_var = "The cat strolls by"
+
+# print(len(random_var))
+
+# num_list = [12, 13, 14]
+
+# Old way
+# total = 0
+# for num in num_list:
+#     # total = total + num
+#     total += num
+#     print(f"Current total: {total}")
+
+# with sum() function
+# print(sum(num_list))
+
+# Example 2
+# def random_phrase(user_phrase):
+#     print(f"{user_phrase} Dumbledore said calmly!")
+
+
+# user_prompt = input("Enter a random phrase: ")
+
+# random_phrase(user_prompt)
+
+# def add(num1, num2):
+#     return num1 + num2
+
+
+# num1_prompt = int(input("Enter first number: "))
+# num2_prompt = int(input("Enter second number: "))
+
+# new_sum = add(num1_prompt, num2_prompt)
+# print(new_sum)
