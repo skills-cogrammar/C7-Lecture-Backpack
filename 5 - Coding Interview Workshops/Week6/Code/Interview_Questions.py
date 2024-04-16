89ca1a5f9c2d8ebc385f8422d4737d91191f1a99
Anri-Lombard <anri.m.lombard@gmail.com>
2024-04-16 19:04:37 +0200

    Interview questions

---
+++ 5 - Coding Interview Workshops/Week6/Code/Interview_Questions.py
@@ -0,0 +1,15 @@
+# Question 1: Implement a stack using a Python list. Include push, pop,
+# and is_empty methods.
+
+
+
+# Question 2: Implement a function that checks if a given string of
+# parentheses is balanced using a stack.
+
+
+
+# Question 3: Implement a function called apply_operation that takes a
+# function as an argument and returns a new function that applies the
+# given function to its argument and doubles the result.
+
+
