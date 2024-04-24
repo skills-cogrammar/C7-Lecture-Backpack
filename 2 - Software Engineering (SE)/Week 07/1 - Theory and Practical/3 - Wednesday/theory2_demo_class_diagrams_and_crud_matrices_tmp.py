81ca1271f3b27784c8d55d7b2e7e375fe9dad148
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2024-04-24 20:00:08 +0200

    Uploaded Demo Template

---
+++ 2 - Software Engineering (SE)/Week 07/1 - Theory and Practical/3 - Wednesday/theory2_demo_class_diagrams_and_crud_matrices_tmp.py
@@ -0,0 +1,56 @@
+# ******* You are welcome to use draw.io and download it to your desktop *******
+# Download desktop draw.io from https://github.com/jgraph/drawio-desktop/releases
+
+"""     ========= Design Scenario =========
+Design a simple Library System for a local library. 
+The system should allow librarians to manage book inventory, 
+handle borrowing and returning books, and keep track of library members.
+"""
+
+"""     -------- Class Diagram --------
+
+
+        -- Class Descriptions:
+
+
+        -- Relationships:
+
+
+
+        -- The Diagram:
+
+
+
+"""
+
+"""     -------- CRUD Matrix --------
+•	Create a CRUD Matrix to outline the CRUD operations 
+    for each class in your Class Diagram.
+•	Specify the Create, Read, Update, and Delete operations 
+    for classes like Book, Member, and BorrowingRecord.
+•	Consider who has permission to perform each operation 
+    (e.g., Librarian can add, update, and delete Books, 
+    while Members can only read and borrow Books).
+•	Provide justification for the access permissions assigned to each operation.
+
+CRUD Matrix for Library System Classes (Basic and Roll-Based Hybrid)
+
+
+"""
+#     -------- MVC Pattern --------
+
+# Model
+
+
+# View
+
+
+# Controller
+
+
+# Example usage
+
+
+
+# Resource below for an advance library management system
+# https://www.gleek.io/blog/library-management-system
