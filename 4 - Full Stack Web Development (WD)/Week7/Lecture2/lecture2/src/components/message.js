a1903502c38c15ba7fd99854805f897f34f8082b
zahra_moham <zahram@cogrammar.com>
2024-04-28 13:00:48 +0200

    WD Lecture 2 Week 7 Code files

---
+++ 4 - Full Stack Web Development (WD)/Week7/Lecture2/lecture2/src/components/message.js
@@ -0,0 +1,6 @@
+function Message (prop) {
+    return <p> We are happy to have you in our lecture. Please let us know if you have any questions. <br></br>
+    The topic for today's lecture is {prop.topic}. </p>
+}
+
+export default Message;
---
+++ 4 - Full Stack Web Development (WD)/Week7/Lecture2/lecture2/src/components/welcome.js
@@ -0,0 +1,5 @@
+function Welcome (prop) {
+   return <h1> Welcome to the website for WD Lecture {prop.number}. </h1>
+}
+
+export default Welcome;
