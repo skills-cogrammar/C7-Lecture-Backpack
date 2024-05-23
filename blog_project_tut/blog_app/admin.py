8de12f3c6d4267ca00be3f1240fd7ac797184955
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2024-05-23 23:11:38 +0200

    Delete blog_project_tut directory

--- blog_project_tut/blog_app/admin.py
+++
@@ -1,7 +0,0 @@
-# blog_app/admin.py
-
-from django.contrib import admin
-from .models import BlogPost
-
-# Register your models here.
-admin.site.register(BlogPost)
--- blog_project_tut/blog_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class BlogAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_app'
