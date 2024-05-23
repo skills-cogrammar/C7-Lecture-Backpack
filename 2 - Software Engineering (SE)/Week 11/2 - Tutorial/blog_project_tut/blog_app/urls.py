0e27efff0ea0876da5231a0ff5b402b7f60dcdbe
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2024-05-23 23:15:58 +0200

    Add Tutorial Resources
    blog_project_tut includes App Guide until Stage 7

---
+++ 2 - Software Engineering (SE)/Week 11/2 - Tutorial/blog_project_tut/blog_app/urls.py
@@ -0,0 +1,11 @@
+# blog_app/urls.py
+
+from django.urls import path
+from . import views     # from .views import index
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_post, name='add_post'),
+    path('post/<int:post_id>/', views.view_post, name='view_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+]
