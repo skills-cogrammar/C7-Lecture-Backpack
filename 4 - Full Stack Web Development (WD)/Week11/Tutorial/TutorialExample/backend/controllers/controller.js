50eb63c636c14f24d899eb77f109e71c889e283f
Dan-Hyperion <danw@hyperiondev.com>
2024-05-26 18:14:04 +0300

    WD WK11 tutorial edit

--- 4 - Full Stack Web Development (WD)/Week11/Tutorial/TutorialExample/backend/controllers/controller.js
+++ 4 - Full Stack Web Development (WD)/Week11/Tutorial/TutorialExample/backend/controllers/controller.js
@@ -9,7 +9,7 @@ const helloWorld = (req, res)=>{
 //C(Create)
 const createBlogs = async (req, res)=>{
     const { title, content, author } = req.body
-    const blog = await BlogModel.create({
+    const blog = await BlogModel.create({ //creates a blogs
         title: title,
         content: content,
         author: author
@@ -24,12 +24,38 @@ const createBlogs = async (req, res)=>{
 
 //R(read)
 const readBlogs = async (req, res)=>{
-    const blogs = await BlogModel.find()
+    const blogs = await BlogModel.find() //finds all blogs
     res.json(blogs)
+}
+
+//U(update)
+const updateBlog = async (req, res)=>{
+    const { id } = req.params
+    const values = req.body
+
+    const blog = await BlogModel.findByIdAndUpdate(id, values)
+    res.json({
+        message: "Blog Updated successfully.",
+        blog: blog
+    })
+}
+
+//D(delete)
+const deleteBlog = async (req, res)=>{
+    const { id } = req.params 
+    const blog = await BlogModel.findByIdAndDelete(id)
+
+    res.json({
+        message: "Blog deleted successfully",
+        blog: blog
+    })
+
 }
 
 module.exports = {
     helloWorld,
     readBlogs,
-    createBlogs
+    createBlogs,
+    updateBlog,
+    deleteBlog
 }
--- 4 - Full Stack Web Development (WD)/Week11/Tutorial/TutorialExample/backend/routes/route.js
+++ 4 - Full Stack Web Development (WD)/Week11/Tutorial/TutorialExample/backend/routes/route.js
@@ -3,9 +3,12 @@ const controllers = require('../controllers/controller')
 
 const routes = express.Router()
 
+//All API routes defined here
 routes.get('/', controllers.helloWorld)
 routes.get('/blogs', controllers.readBlogs)
 routes.post('/blogs/create', controllers.createBlogs)
+routes.put('/blogs/update/:id', controllers.updateBlog)
+routes.delete('/blogs/delete/:id', controllers.deleteBlog)
 
 module.exports = {
     routes
