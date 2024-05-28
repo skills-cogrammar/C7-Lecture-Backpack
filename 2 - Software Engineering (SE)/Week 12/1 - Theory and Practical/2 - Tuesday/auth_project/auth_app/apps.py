9d89b191fe742b2c30f42db1b90f59906c078e12
riaandeventer-cogrammar <152322616+riaandeventer-cogrammar@users.noreply.github.com>
2024-05-28 22:46:43 +0200

    Added Theory1 - Django Authentication

---
+++ 2 - Software Engineering (SE)/Week 12/1 - Theory and Practical/2 - Tuesday/auth_project/auth_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'auth_app'
---
+++ 2 - Software Engineering (SE)/Week 12/1 - Theory and Practical/2 - Tuesday/auth_project/auth_app/views.py
@@ -0,0 +1,51 @@
+# auth_app/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def index(request):
+    return render(request, 'auth_app/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'auth_app/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}.')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    form = AuthenticationForm()
+    return render(request, 'auth_app/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'You have successfully logged out.')
+    return redirect('index')
+
+def protected_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'auth_app/protected.html')
