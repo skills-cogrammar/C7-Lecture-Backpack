facefbee1839b5de78cae7dc85272005ce0b89c1
1r8c0d3 <bonaventureg@hyperiondev.com>
2024-06-26 23:27:39 +0300

    add intro to GraphQL slides and code example

---
+++ 4 - Full Stack Web Development (WD)/Week16/Lesson2/client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// setup ApolloClient
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', // the reason for returning `undefined` -> I had put url instead of uri
+  cache: new InMemoryCache()
+})
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
